cb24fefde595f482c6dc9d13816734803feb3866
Thiago Fonseca <thicorfon@gmail.com>
2018-10-03 16:33:18 -0300

    Nail polish

--- constants.py
+++ constants.py
@@ -1,7 +1,11 @@
 uppercase = [chr(x) for x in range(65,91)]
 lowercase = [chr(x) for x in range(97,123)]
 digits = [chr(x) for x in range(48,58)]
-special = ['!','@','#','%','&','*','(',')','_','+','+','-','=','{','[','}',']','?','/','`',"'","^",'~','<',',','>','.',':',';','|','\\','"']
+special = ['!','@','#','%','&','*','(',')','_','+',
+		   '+','-','=','{','[','}',']','?','/','`',
+		   "'","^",'~','<',',','>','.',':',';','|',
+		   '\\','"']
 palavrasPredefinidas = ["SIN","COS","TAN","ATN","EXP","ABS","LOG","SQR","INT","RND"]
-palavrasReservadas = ["END","LET","FN","READ","DATA","PRINT","GOTO","IF","THEN","FOR","TO","STEP","NEXT","DIM","DEF FN","GOSUB","RETURN","REM"]+palavrasPredefinidas
+palavrasReservadas = ["END","LET","FN","READ","DATA","PRINT","GOTO","IF","THEN","FOR",
+					  "TO","STEP","NEXT","DIM","DEF FN","GOSUB","RETURN","REM"]+palavrasPredefinidas
 
